6014a8db45008d235130583a1c5fef361ebef14f
Adarsh_Ar <138155091+Adarsh-Arbrand@users.noreply.github.com>
2024-03-14 16:07:45 +0530

    Update form.php

--- form.php
+++ form.php
@@ -6,7 +6,7 @@ $email= $_POST['email'];
 $to = "ranjanadarsh@mail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email;
-$headers = "From: noreply@.com" . "\r\n" .
+$headers = "From: adarsh-arbrand.github.io" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
