45e1ac7c9ce8d9925c7ac38b1ba43b086f3c680d
Adarsh_Ar <138155091+Adarsh-Arbrand@users.noreply.github.com>
2024-08-18 16:25:54 +0530

    Update form.php

--- form.php
+++ form.php
@@ -9,8 +9,26 @@ $con = mysqli_connect($server, $username, $password, $dbname);
 if(!$con){
     echo "not connected";
 }
-else{
-    echo "connect";
+
+$Name =$_POST['name'];
+$gametag =$_POST['gametag'];
+$country =$_POST['country'];
+$email =$_POST['email'];
+$year =$_POST['year'];
+$best =$_POST['best'];
+$sql = INSERT INTO `user_data`(`NAME`, `GameTag`, `COUNTRY`, `Email`, `How Many Year have you Been Playing Minecraft`, `Menorable Day`) VALUES ('$name','$gametag','$country','$email','$year','$best')
+
+$result = mysqli_query($con , $sql);
+
+if($result)
+{
+    echo "data submited";
+
 }
+else 
+{
+    echo "error";
+}
+
 
 ?>
